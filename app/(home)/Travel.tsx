634597acb38698088aff388c66b5fb676d5c18fa
Alexander <aleksievisa@gmail.com>
2023-05-13 04:49:14 +0300

    feat: add fn req data

--- app/(home)/Travel.tsx
+++ app/(home)/Travel.tsx
@@ -1,10 +1,15 @@
+import { Post } from "@prisma/client";
 import { Card } from "../(shared)/Card"
 
-export const Travel = () => {
-  return (
-    <section className={'mt-10'}>
-      <hr className="border-1" />
+type Props = {
+  travelPosts: Array<Post>;
+};
 
+export const Travel = ({ travelPosts }: Props) => {
+  return (
+    <section className="mt-10">
+      <hr className="border-1" />
+      {/* HEADER */}
       <div className="flex items-center gap-3 my-8">
         <h4 className="bg-accent-green py-2 px-5 text-wh-900 text-sm font-bold">
           TRAVEL
@@ -12,24 +17,29 @@ export const Travel = () => {
         <p className="font-bold text-2xl">New Travel Experiences</p>
       </div>
 
-      <div className={'sm:flex justify-between gap-8'}>
+      {/* CARDS ROW */}
+      <div className="sm:flex justify-between gap-8">
         <Card
-          className={'basis-1/3 bg-wh-500 mt-5 sm:mt-0'}
+          className="basis-1/3 mt-5 sm:mt-0"
           imageHeight="h-80"
+          post={travelPosts[0]}
         />
         <Card
-          className={'basis-1/3 bg-wh-500 mt-5 sm:mt-0'}
+          className="basis-1/3 mt-5 sm:mt-0"
           imageHeight="h-80"
+          post={travelPosts[1]}
         />
         <Card
-          className={'basis-1/3 bg-wh-500 mt-5 sm:mt-0'}
+          className="basis-1/3 mt-5 sm:mt-0"
           imageHeight="h-80"
+          post={travelPosts[2]}
         />
       </div>
       <Card
-        className={'bg-wh-500 sm:flex justify-between items-center gap-3 mt-7 mb-5'}
+        className=" sm:flex justify-between items-center gap-3 mt-7 mb-5"
         imageHeight="h-80"
+        post={travelPosts[3]}
       />
     </section>
-  )
-}
+  );
+};
--- app/(shared)/Other.tsx
+++ app/(shared)/Other.tsx
@@ -1,8 +1,11 @@
+import { Post } from "@prisma/client";
 import { Card } from "./Card"
 
- 
+type Props = {
+  otherPosts: Array<Post>;
+};
 
-export const Other = () => {
+export const Other = ({ otherPosts }: Props) => {
   return (
     <section className="pt-4 mb-16">
       <hr className="border-1" />
@@ -10,22 +13,26 @@ export const Other = () => {
       <p className="font-bold text-2xl my-8">Other Trending Posts</p>
       <div className="sm:grid grid-cols-2 gap-16">
         <Card
-          className="bg-wh-500 mt-5 sm:mt-0"
+          className="mt-5 sm:mt-0"
           imageHeight="h-80"
+          post={otherPosts[0]}
         />
         <Card
-          className="bg-wh-500 mt-5 sm:mt-0"
+          className="mt-5 sm:mt-0"
           imageHeight="h-80"
+          post={otherPosts[1]}
         />
         <Card
-          className="bg-wh-500 mt-5 sm:mt-0"
+          className="mt-5 sm:mt-0"
           imageHeight="h-80"
+          post={otherPosts[2]}
         />
         <Card
-          className="bg-wh-500 mt-5 sm:mt-0"
+          className="mt-5 sm:mt-0"
           imageHeight="h-80"
+          post={otherPosts[3]}
         />
       </div>
     </section>
   );
-}
+};
